ec22ca9c21a13033ef0d0cb3bc815001f0401c1a
Viyerelu23333 <11363469+Viyerelu23333@users.noreply.github.com>
2024-04-29 16:28:28 -0500

    feat: clighter redex

---
+++ clighter.rkt
@@ -0,0 +1,64 @@
+#lang racket
+
+(require redex)
+
+(define-language Clighter
+  ;;; Abs Syntax
+  (signedness ::= Signed Unsigned)
+  (intsize    ::= I8 I16 I32)
+  (floatsize  ::= F32 F64)
+  (n          ::= integer)
+  (f          ::= float)
+  (id         ::= variable-not-otherwise-mentioned)
+  (τ          ::= (int intsize signedness)
+                  (float floatsize)
+                  (void)
+                  (array τ n)
+                  (pointer τ)
+                  (function τ τ ...)
+                  (struct id φ ...)
+                  (union id φ ...)
+                  (comppointer id))
+  (φ          ::= (id τ))
+
+  ;;; Exprs
+  (a          ::= id
+                  n
+                  f
+                  (sizeof τ)
+                  (uop a)
+                  (bop a a)
+                  (* a)
+                  (@ a id)
+                  (& a)
+                  ((τ) a)
+                  (? a a a))
+  (uop        ::= - ~ !)
+  (bop        ::= + - * / %
+                  << >> & \| ^
+                  < <= > >= == !=)
+
+  ;; Stmts
+  (s          ::= skip
+                  (= a a)
+                  (= a (a a ...))
+                  (a a ...)
+                  (s ...)
+                  (if a s1 s2)
+                  (switch a sw)
+                  (while a s)
+                  (do-while s a)
+                  (for s a s s)
+                  break
+                  continue
+                  return
+                  (return a))
+  (sw         ::= (default s) (case n (s sw)))
+
+  ;;; Fun & Prog
+  (dcl        ::= (τ id))
+  (F          ::= (τ id dcl ... dcl ... s))
+  (Fe         ::= (extern τ id dcl ...))
+  (Fd         ::= F Fe)
+  (P          ::= (dcl ... Fd ...)) ; main = id
+)
